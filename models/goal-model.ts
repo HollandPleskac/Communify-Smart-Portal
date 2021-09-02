f2fb0cf941d60687c08836045a6affc50c18966a
Holland Pleskac <hollandpleskac@gmail.com>
2021-09-01 20:00:26 -0700

    clicking on stuff on the dashboard

--- models/goal-model.ts
+++ models/goal-model.ts
@@ -4,13 +4,15 @@ class Goal {
   estimatedFinishDate: string
   completedProjects: number
   unFinishedProjects: number
+  id: number
 
-  constructor(name: string, startDate: string, estimatedFinishDate: string, completedProjects:number, unFinishedProjects:number) {
+  constructor(name: string, startDate: string, estimatedFinishDate: string, completedProjects:number, unFinishedProjects:number, id:number) {
     this.name = name
     this.startDate = startDate
     this.estimatedFinishDate = estimatedFinishDate
     this.completedProjects = completedProjects
     this.unFinishedProjects = unFinishedProjects
+    this.id = id
   }
 }
 
